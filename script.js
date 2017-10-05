edca7c01741c12ba72d41ae3c8444ff9a1862bb5
michellejanosi <michelle.janosi@gmail.com>
2017-10-05 17:03:57 -0400

    Added images and a few styles

--- script.js
+++ script.js
@@ -4,23 +4,6 @@ let factText = document.querySelector('#factText');
 
 let numberInput = document.querySelector('#numberInput');
 numberInput.addEventListener('input', getFactFetch);
-// numberInput.addEventListener('input', getFactAjax);
-
-// function getFactAjax() {
-//   let number = numberInput.value;
-//
-//   let xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'http://numbersapi.com/' + number);
-//
-//   xhr.onload = function() {
-//     if (this.status == 200 && number != '') {
-//       fact.style.display = 'block';
-//       factText.innerText = this.responseText;
-//     }
-//   }
-//
-//   xhr.send();
-// }
 
 function getFactFetch() {
   let number = numberInput.value;
